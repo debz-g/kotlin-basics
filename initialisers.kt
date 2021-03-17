67bd5edf36ae1b489c020adc82834a5e7d425b6d
Debayan Ghosh Dastider <debz.exe@gmail.com>
2021-03-18 01:11:21 +0530

    Create initialisers.kt

---
+++ initialisers.kt
@@ -0,0 +1,12 @@
+fun main()
+{
+var a= person("Debayan","Ghosh")
+}
+
+class person(first: String, last: String)
+{
+    init {
+        println("First Name: $first \n Last name: $last") //This will get executed class is called
+    }
+
+}
